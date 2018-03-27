11d15f4ebf5f76324d8a3119706259b50e30c6ca
sretundijr <sretundijr@gmail.com>
2018-03-27 01:32:24 -0600

    player now stores their moves

--- src/player.js
+++ src/player.js
@@ -5,12 +5,16 @@ export default class Player {
       moves: [],
     }
     this.getSymbol = this.getSymbol.bind(this);
+    this.getMoves = this.getMoves.bind(this);
   }
   incrementMoves(index) {
     this.state.moves.push(index);
   }
   getSymbol() {
     return this.state.symbol;
+  }
+  getMoves() {
+    return this.state.moves;
   }
   move(index) {
     this.incrementMoves(index);
