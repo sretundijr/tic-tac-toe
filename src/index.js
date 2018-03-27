d08691ce8f58fd81b966e0dc41461fe55c4aeeb9
sretundijr <sretundijr@gmail.com>
2018-03-26 18:25:59 -0600

    beginning game play loop

--- src/index.js
+++ src/index.js
@@ -1,46 +1,108 @@
 import './styles.css';
 
-const gameBtnTemplate = (index) => {
-  return `<button id="${index}-btn" class="game-btn" value=""></button>`
+const gameBtnTemplate = (index, value = '') => {
+  return `<button id="${index}-btn" class="game-btn">${value}</button>`
 }
 
-const gameBoardBtn = () => {
-  const btnArray = [];
-  for (let i = 0; i < 9; i++) {
-    btnArray.push(gameBtnTemplate(i));
-  }
+const gameBoardBtn = (board) => {
+  const btnArray = board.map((item, index) => {
+    if (item) {
+      return gameBtnTemplate(index, item.getSymbol());
+    }
+    return gameBtnTemplate(index);
+  })
   return btnArray.join('');
 }
 
-const gameBoardBtnEvent = () => {
-  let btnIndex = '';
+const gameBoardBtnEvent = (Game) => {
   document.getElementById('game-board').addEventListener('click', (e) => {
-    btnIndex = e.target.id.replace('-btn', '');
+    const btnIndex = parseInt(e.target.id.replace('-btn', ''), 10);
+    // player.incrementMoves();
+    // board.setPlayerPosition(btnIndex, player);
+    // console.log(board.getGameBoard());
+    console.log(Game.state.Human);
+    // Game.play(btnIndex);
+    document.getElementById('game-board').innerHTML = gameBoardBtn(Game.play(btnIndex));
   })
-  return btnIndex;
+}
+
+class GamePlayLoop {
+  constructor(Board, Human, Ai) {
+    this.state = {
+      Board,
+      Human,
+      Ai,
+      totalMoves: 0,
+    };
+  }
+
+  play(btnIndex) {
+    if (this.state.totalMoves % 2 === 0) {
+      this.state.Board.setPlayerPosition(btnIndex, this.state.Human);
+      this.state.Human.move();
+      console.log(this.state.Human);
+      console.log(this.state.Board);
+
+    } else {
+      this.state.Board.setPlayerPosition(btnIndex, this.state.Ai);
+      this.state.Ai.move();
+
+      console.log(this.state.Ai);
+    }
+    this.state.totalMoves++;
+    return this.state.Board.getGameBoard();
+  }
+
+  incrementGameBoard() {
+
+  }
+
+  togglePlayer(currentPlayer) {
+
+  }
 }
 
 class GameBoard {
   constructor() {
     this.state = {
-      gameBoard: [],
+      gameBoard: ['', '', '', '', '', '', '', '', ''],
     };
   };
   setPlayerPosition(index, PlayerObj) {
     this.state.gameBoard[index] = PlayerObj;
   }
+  getGameBoard() {
+    return this.state.gameBoard;
+  }
 }
 
 class Player {
-  constructor() {
+  constructor(symbol) {
     this.state = {
-      symbol: '',
-      numberOfMoves: '',
+      symbol,
+      numberOfMoves: 0,
     }
+  }
+  incrementMoves() {
+    this.state.numberOfMoves++;
+  }
+  getSymbol() {
+    return this.state.symbol;
+  }
+  move() {
+    this.incrementMoves();
+    return this.getSymbol();
   }
 }
 
 document.addEventListener('DOMContentLoaded', () => {
-  document.getElementById('game-board').innerHTML = gameBoardBtn();
-  gameBoardBtnEvent();
+  const Game = new GameBoard();
+  document.getElementById('game-board').innerHTML = gameBoardBtn(Game.getGameBoard());
+  const humanPlayer = new Player('X');
+  const aiPlayer = new Player('O');
+
+  const GamePlay = new GamePlayLoop(Game, humanPlayer, aiPlayer);
+
+  // fist move to human
+  gameBoardBtnEvent(GamePlay);
 })
