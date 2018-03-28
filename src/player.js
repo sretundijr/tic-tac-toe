143023396370a8b89b2d7d41964b14401685b239
sretundijr <sretundijr@gmail.com>
2018-03-28 01:13:27 -0600

    added methods

--- src/player.js
+++ src/player.js
@@ -6,6 +6,11 @@ export default class Player {
     }
     this.getSymbol = this.getSymbol.bind(this);
     this.getMoves = this.getMoves.bind(this);
+    this.move = this.move.bind(this);
+    this.setMoves = this.setMoves.bind(this);
+  }
+  setMoves(list) {
+    this.state.moves = list;
   }
   incrementMoves(index) {
     this.state.moves.push(index);
