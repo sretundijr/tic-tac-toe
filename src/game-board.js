98bb1ce3416a6e9857033234d2b2f1af123ce692
sretundijr <sretundijr@gmail.com>
2018-03-27 22:15:47 -0600

    refactor winning row to regex

--- src/game-board.js
+++ src/game-board.js
@@ -13,38 +13,46 @@ export default class GameBoard {
       PlayerObj,
     }
   }
+  // game board is only used for rendering?
   getGameBoard() {
     return this.state.gameBoard;
   }
-  winningRows(player) {
-    let row1 = 0, row2 = 0, row3 = 0;
-    for (let i = 0; i < this.state.gameBoard.length; i++) {
-      if (this.state.gameBoard[i] && this.state.gameBoard[i].getSymbol() === player.getSymbol()) {
-        if (i < 3) {
-          row1++;
-        } else if (i < 6) {
-          row2++;
-        } else {
-          row3++;
-        }
-      }
-    }
-    if (row1 === 3 || row2 === 3 || row3 === 3) {
-      return true;
-    }
-    return false;
+  // todo check for win using regex
+  winningRows(listOfMoves) {
+    // let row1 = 0, row2 = 0, row3 = 0;
+    // for (let i = 0; i < this.state.gameBoard.length; i++) {
+    //   if (this.state.gameBoard[i] && this.state.gameBoard[i].getSymbol() === player.getSymbol()) {
+    //     if (i < 3) {
+    //       row1++;
+    //     } else if (i < 6) {
+    //       row2++;
+    //     } else {
+    //       row3++;
+    //     }
+    //   }
+    // }
+    // if (row1 === 3 || row2 === 3 || row3 === 3) {
+    //   return true;
+    // }
+    // return false;
+    const rows = [
+      /[012]/g,
+      /[345]/g,
+      /[678]/g,
+    ];
+    return this.isWinningCombo(rows, listOfMoves)
   }
-  winningColumns(player) {
+  winningColumns(listOfMoves) {
     const columns = [
       /[036]/g,
       /[147]/g,
       /[258]/g,
     ]
-    return this.isWinningCombo(columns, player);
+    return this.isWinningCombo(columns, listOfMoves);
   }
 
-  isWinningCombo(winningCombos, player) {
-    const playerMoveString = player.getMoves().join('');
+  isWinningCombo(winningCombos, listOfMoves) {
+    const playerMoveString = listOfMoves.join('');
     const playerColumns = winningCombos.map((item) => {
       return playerMoveString.match(item);
     })
@@ -59,17 +67,18 @@ export default class GameBoard {
     return winnerWinner;
   }
 
-  diagnolWin(player) {
+  diagnolWin(listOfMoves) {
     const angles = [
       /[048]/g,
       /[246]/g,
     ];
 
-    return this.isWinningCombo(angles, player);
+    return this.isWinningCombo(angles, listOfMoves);
   }
 
   isWinner(player) {
-    if (this.winningRows(player) || this.winningColumns(player) || this.diagnolWin(player)) {
+    if (this.winningRows(player.getMoves()) ||
+      this.winningColumns(player.getMoves()) || this.diagnolWin(player.getMoves())) {
       return true;
     }
     return false;
